0231929d547afd2c83b8b523b8f76ddf6238661c
Jakub D <mrbestkubek@gmail.com>
2019-12-10 00:26:54 +0100

    changes in config

--- src/main/java/spring/hibernate/HibernatePSConfig.java
+++ src/main/java/spring/hibernate/HibernatePSConfig.java
@@ -20,7 +20,7 @@ public class HibernatePSConfig {
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "org.postgresql.Driver");
 
-                URI dbUri = new URI("postgresql://jpleadtbxxfbyd:a0264654d3a3d2b2a211244b18f9cfc548d21646a087dea16f403eb658ce21ed@ec2-46-137-120-243.eu-west-1.compute.amazonaws.com:5432/d4mb0i32pjb4pi");
+                URI dbUri = new URI("postgres://mutcpxoggvfnyc:ebcb7793c6de62c22b058595b05b8e01ff4f83de6db2f2677481946d22d2ddfd@ec2-54-247-96-169.eu-west-1.compute.amazonaws.com:5432/d1tjug5h1o8q5o");
 
                 String username = dbUri.getUserInfo().split(":")[0];
                 String password = dbUri.getUserInfo().split(":")[1];
